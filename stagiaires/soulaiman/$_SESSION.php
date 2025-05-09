098c1ed8f0c3a72fe5887914aee351acdc323e36
souli <chair.soulaiman.map@gmail.com>
2025-05-09 13:24:35 +0200

    exe2 corriction

--- stagiaires/soulaiman/$_SESSION.php
+++ stagiaires/soulaiman/$_SESSION.php
@@ -11,7 +11,7 @@ if (!isset($_SESSION['arrivee'])) {
 
     $_SESSION['compte'] = 1;
 }
-    $pluriel = $_SESSION['compte'] > 1 ? "s" : ""; 
+    $pluriel = time() - $_SESSION['arrivee'] > 0 ? "s" : ""; 
 ?>
 
 <!DOCTYPE html>
