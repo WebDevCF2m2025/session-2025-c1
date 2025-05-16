9092fc86947b96d008eefa9f053e9eeb3b03b54c
WebDevCF2m <gitweb@cf2m.be>
2025-05-16 12:28:19 +0200

    config-dev

---
+++ exemple5/config-dev.php
@@ -0,0 +1,10 @@
+<?php
+const DB_DRIVER = "mysql";
+const DB_HOST = "localhost";
+const DB_NAME = "exemple5";
+const DB_PORT = 3306;
+const DB_CHARSET = "utf8";
+const DB_LOGIN = "root";
+const DB_PWD = "";
+
+const DB_DSN = DB_DRIVER.":host=".DB_HOST.";port=".DB_PORT.";dbname=".DB_NAME.";charset=".DB_CHARSET;
